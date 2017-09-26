91863383efa2c6b5c1ba88f0bd829ef821f8666d
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-09-26 21:52:50 +0200

    Update to use the v7.preference library

--- PreferenceTest/PreferenceTest/PreferenceTest.Droid/Fragments/PreferenceFragment.cs
+++ PreferenceTest/PreferenceTest/PreferenceTest.Droid/Fragments/PreferenceFragment.cs
@@ -1,24 +1,19 @@
 ﻿using Android.OS;
 using Android.Runtime;
-using Android.Views;
-using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Support.V7.Preference;
 using MvvmCross.Droid.Views.Attributes;
-using MvvmCross.Droid.Views.Fragments;
 using PreferenceTest.Core.ViewModels;
 using PreferenceTest.Core.ViewModels.Fragments;
-using PreferenceTest.Droid.Activities;
 
 namespace PreferenceTest.Droid.Fragments
 {
     [MvxFragmentPresentation(typeof(MainPreferenceViewModel), Resource.Id.content_frame, true)]
     [Register(nameof(PreferenceFragment))]
-    public class PreferenceFragment : MvxPreferenceFragment<PreferenceViewModel>
+    public class PreferenceFragment : MvxPreferenceFragmentCompat<PreferenceViewModel>
     {
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
         {
-            base.OnCreateView(inflater, container, savedInstanceState);
-            var view = this.BindingInflate(Resource.Xml.preferences, container, false);
-            return view;
+            SetPreferencesFromResource(Resource.Xml.preferences, rootKey);
         }
     }
 }
